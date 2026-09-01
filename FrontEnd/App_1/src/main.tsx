549ba0ca6f266e6a489cb042c2fb789a0de2a65e
RDLSMODERNIZACION <modernizacion.mrdls@gmail.com>
2026-09-01 00:56:08 -0300

    feat: open pump analysis from full KPI row

--- FrontEnd/App_1/src/main.tsx
+++ FrontEnd/App_1/src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { installPumpRowNavigation } from "@/lib/pumpRowNavigation";
+
+installPumpRowNavigation();
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
